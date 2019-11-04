ff64f40ec2926cc307f51cefb5a50af099be45cc
Erika Jenkins <erikamjenkins@ufl.edu>
2019-11-04 10:19:06 -0500

    corrected "for" loop for the event listener

--- js/main.js
+++ js/main.js
@@ -12,6 +12,6 @@ function loopThrough() {
 };
 
 //The Event Listener
-for(let i = 0; i = myBoxes.length; i++){
+for(let i = 0; i < myBoxes.length; i++){
   myBoxes[i].addEventListener('click', loopThrough);
 };
